2dd66239a0fa89fea518b1e3bcd1917e557d4d1e
luamas <myluamas@gmail.com>
2020-04-20 22:11:57 +0800

    fix

--- hello-action/src/main/kotlin/icons/SdkIcons.kt
+++ hello-action/src/main/kotlin/icons/SdkIcons.kt
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader
 
 object SdkIcons {
     @JvmField
-    open val Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg")
+    val Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg")
 }
